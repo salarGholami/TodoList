ae5865aecbe1c097d853679f965a74a04c256491
Salar Gholami <salargholami021@gmail.com>
2023-02-27 04:06:19 +0330

    general structure

---
+++ src/components/TodoApp.jsx
@@ -0,0 +1,14 @@
+import TodoForm from "./TodoForm";
+import TodoList from "./TodoList";
+
+const TodoApp = () => {
+//   const [todos, setTodos] = useState([]);
+  return (
+    <div className="container">
+      <TodoForm />
+      <TodoList />
+    </div>
+  );
+};
+
+export default TodoApp;
